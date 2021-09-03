9caf7b84e7372bf993c33bf9d64f8bbdf87f37b4
Jabari-Finney <jabaritf@gmail.com>
2021-09-03 17:30:13 -0400

    mvp

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,70 @@
 // Write your "projects" router here!
+const express = require("express");
+
+const Projects = require("./projects-model");
+
+const {
+    validateProjID,
+    validateProjBody,
+    validateProjUpdate,
+} = require("./projects-middleware");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Projects.get()
+    .then((projects) => {
+      if (!projects) {
+        res.send([]);
+      } else {
+        res.status(200).json(projects);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: err.message,
+      });
+    });
+});
+
+router.get("/:id", validateProjID, (req, res, next) => {
+  res.status(200).json(req.project);
+});
+
+router.post("/", validateProjBody, (req, res, next) => {
+  Projects.insert(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(next);
+});
+
+router.put("/:id", validateProjID, validateProjUpdate, (req, res, next) => {
+  Projects.update(req.params.id, req.body)
+    .then((updated) => {
+      res.status(200).json(updated);
+    })
+    .catch(next);
+});
+
+router.delete("/:id", validateProjID, (req, res, next) => {
+  Projects.remove(req.params.id)
+    .then(() => {
+      res.status(200).json({ message: "project deleted" });
+    })
+    .catch(next);
+});
+
+router.get("/:id/actions", validateProjID, (req, res, next) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      if (!actions) {
+        res.send([]);
+      } else {
+        res.status(200).json(actions);
+      }
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
+
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionsRouter);
+
+server.use("*", (req, res, next) => {
+  res.json({
+    message: "Fail",
+  });
+});
 
 module.exports = server;
